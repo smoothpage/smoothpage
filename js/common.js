fbdf1c866c536fac42120e75b24edaeec2fffbd0
Dimitri <path.demons@gmail.com>
2018-12-09 16:54:02 +0100

    commit 2

--- js/common.js
+++ js/common.js
@@ -1,10 +1,28 @@
-$('.overlay-link__preview-link').click(function(){
-	$('.frame-wrap, .myframe').show();
-	$('body').addClass('hidden-y');
-});
+$(document).ready(function() {
 
-$('#close-frame').click(function(){
-	$('.frame-wrap').hide(300);
-	$('body').removeClass('hidden-y');
-});
+	$('.overlay-link__preview-link').click(function(){
+		$('.frame-wrap, .myframe').show();
+		$('body').addClass('hidden-y');
+	});
 
+	$('#close-frame').click(function(){
+		$('.frame-wrap').hide(300);
+		$('body').removeClass('hidden-y');
+	});
+
+// burger
+(function topMenuResponse(){
+
+	var topMenu = $(".header"),
+	buttonResponseToggle = topMenu.find(".burger-button");
+	navResponseToggle = topMenu.find(".nav-menu");
+
+	buttonResponseToggle.on('click', function(){
+		navResponseToggle.slideToggle();
+		$(".header").toggleClass("push-wrap");
+
+	});
+
+})();
+// end - burger
+});
